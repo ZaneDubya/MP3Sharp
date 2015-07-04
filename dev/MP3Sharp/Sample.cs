cc8cfe5ec93b97fbb6be2f000a9b5f674d1ab1a9
Zane Wagner <ZaneDubya@users.noreply.github.com>
2015-07-04 14:08:11 -0500

    Cleanup sample.cs

--- dev/MP3Sharp/Sample.cs
+++ dev/MP3Sharp/Sample.cs
@@ -1,3 +1,19 @@
+// /***************************************************************************
+//  *   Sample.cs.cs
+//  *   Copyright (c) 2015 Zane Wagner, Robert Burke,
+//  *   the JavaZoom team, and others.
+//  * 
+//  *   All rights reserved. This program and the accompanying materials
+//  *   are made available under the terms of the GNU Lesser General Public License
+//  *   (LGPL) version 2.1 which accompanies this distribution, and is available at
+//  *   http://www.gnu.org/licenses/lgpl-2.1.html
+//  *
+//  *   This library is distributed in the hope that it will be useful,
+//  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//  *   Lesser General Public License for more details.
+//  *
+//  ***************************************************************************/
 using System;
 
 namespace MP3Sharp
@@ -7,17 +23,17 @@ namespace MP3Sharp
 	/// </summary>
 	internal class Sample
 	{
-        static void Main(string[] args)
+	    public static readonly string Mp3FilePath = @"Sample.mp3";
+
+	    static void Main(string[] args)
         {
             Console.WriteLine("Begin read...");
-            Sample.ReadAllTheWayThroughMp3File();
+            ReadAllTheWayThroughMp3File();
             Console.WriteLine("... end!");
             Console.ReadKey();
         }
 
-		public static readonly string Mp3FilePath = @"Sample.mp3";
-
-		/// <summary>
+	    /// <summary>
 		/// Sample showing how to read through an MP3 file and obtain its contents as a PCM byte stream.
 		/// </summary>
 		public static void ReadAllTheWayThroughMp3File()
@@ -37,6 +53,5 @@ namespace MP3Sharp
 			}
 			Console.WriteLine("Read a total of " + totalBytes + " bytes.");
 		}
-
 	}
 }
