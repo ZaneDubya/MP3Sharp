ce0fb89e2b832f41aaae0996645e71e1ce33e4f7
J Hallam <jesse.colin.hallam@gmail.com>
2015-07-15 20:37:27 -0400

    Added Unit Test project.

---
+++ MP3Sharp.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MP3Sharp.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
